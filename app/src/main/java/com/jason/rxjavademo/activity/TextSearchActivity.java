71160fe4768efd8799da50d74546fb80d11fca30
soaringday <790445149@qq.com>
2016-05-10 15:11:39 +0800

    throttleFirst for prevent double click on button

--- app/src/main/java/com/jason/rxjavademo/activity/TextSearchActivity.java
+++ app/src/main/java/com/jason/rxjavademo/activity/TextSearchActivity.java
@@ -1,11 +1,14 @@
 package com.jason.rxjavademo.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,6 +19,7 @@ import java.util.concurrent.TimeUnit;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 
@@ -25,6 +29,8 @@ public class TextSearchActivity extends AppCompatActivity {
     EditText mEtSearch;
     @Bind(R.id.tv_result)
     TextView mTvResult;
+    @Bind(R.id.fab)
+    FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,22 +63,53 @@ public class TextSearchActivity extends AppCompatActivity {
             }
         }).debounce(500, TimeUnit.MILLISECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Subscriber<String>() {
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        searchText(s);
+                    }
+                });
+        preventMoreClick();
+    }
+
+    private void preventMoreClick() {
+        Observable.create(new Observable.OnSubscribe<Object>() {
             @Override
-            public void onCompleted() {
-
+            public void call(final Subscriber<? super Object> subscriber) {
+                mFab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        subscriber.onNext(v);
+                    }
+                });
             }
+        }).throttleFirst(1, TimeUnit.SECONDS)
+                .subscribe(new Observer<Object>() {
+                    @Override
+                    public void onCompleted() {
 
-            @Override
-            public void onError(Throwable e) {
+                    }
 
-            }
+                    @Override
+                    public void onError(Throwable e) {
 
-            @Override
-            public void onNext(String s) {
-                searchText(s);
-            }
-        });
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+                        Log.d("test", "button clicked");
+                    }
+                });
     }
 
     private void searchText(String text) {
